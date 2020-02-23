3c8918a1f3dd620921461a5c3bb83912e5d41cb2
jeremy10tie <jeremy.gillot@gmail.com>
2020-02-23 22:26:42 +0100

    demo

---
+++ asset/js/main.js
@@ -0,0 +1,42 @@
+$(window).scroll(() => {
+    var scrollTop = $(this).scrollTop();
+    
+    $('#slide2 ').css('top',(scrollTop / -5) + 'px')
+    $('#slide4 ').css('top',-(scrollTop / 6) + 'px')
+})
+
+
+// $(document).ready(function(){
+//     $(window).scroll(function(){
+//         var valeur_scroll = $(window).scrollTop();
+//         console.log(valeur_scroll);
+//         $("#slide2").css('transform',"translateY("+valeur_scroll/1.2 + "px)")
+//     });
+// });
+
+  
+ 
+
+function navMobile()
+{
+    var oppenButton = document.querySelector('.menu_mobile');
+    var logo = document.querySelector('.logo');
+    var menu = document.querySelector('.mobile');
+    var close = document.querySelector('.close_menu');
+
+    oppenButton.addEventListener('click', () => {
+        oppenButton.style.display = 'none';
+        logo.style.display = 'none';
+        menu.style.display = 'block';
+        close.style.display = 'block';
+    })
+
+    close.addEventListener('click', () => {
+        oppenButton.style.display = 'block'; 
+        logo.style.display = 'block';
+        menu.style.display = 'none';
+        close.style.display = 'none';
+    })
+}
+
+navMobile();
